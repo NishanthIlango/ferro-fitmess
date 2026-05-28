81d19a4d0df0b8f524e09b4cd74b9eece7dcb332
Nishanth I <nishant.jcs@gmail.com>
2026-05-29 01:52:43 +0530

    Updated project files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,6 @@ export default function Home() {
         <Hero />
         <About />
         <Services />
-        <Trainers />
         <Transformations />
         <Pricing />
         <Testimonials />
